f57fa2d09d0a3ff454e2b5008a65a6f51decf57b
MatiasIsGood <76411418+MatiasIsGood@users.noreply.github.com>
2023-02-15 12:44:39 +0200

    uusia offsets

--- ac_template/code.cpp
+++ ac_template/code.cpp
@@ -1,60 +1,25 @@
-#include "pch.h"
-
-struct vector3
-{
-	float x;
-	float y;
-	float z;
-};
-
-//todo teleport eteenpäin
-
-#define PITCH 0 //X
-#define YAW 1 //Y
-#define ROLL 2 //Z
-
-bool ChangeGravity = false;
+ï»¿#include "pch.h"
 
 
-void guessNumber(int guess) {
-    
-	std::cout << guess  <<" : ";
-	if (guess == 555) {
-		std::cout << "Correct. You guessed it!\n";
-	}
-	else if (guess < 555) {
-		std::cout << "Your guess is too low.\n";
-	}
-	else if (guess > 555) {
-		std::cout << "Your guess is too high.\n";
-	}
+void __main() {
 
-}
-
-void __main() {	
-
-	//sehän olis muistiosotteet
+	//sehÃ¤n olis muistiosotteet
 	int* ammo = (int*)0x0E0DB40;
 	int* clip = (int*)0x0E0DD40;
 	int* health = (int*)0x0C8155C;
 	float* gravity = (float*)0x01340234; //joku gravity?
 
-	float* z = (float*)0x0E0DA1C;		//ylös
+	float* z = (float*)0x0E0DA1C;		//ylÃ¶s
 	float* y = (float*)(0x0E0DA1C - 4); //oikeelle
-	float* x = (float*)(0x0E0DA1C - 8); //eteenpäi
+	float* x = (float*)(0x0E0DA1C - 8); //eteenpÃ¤i
 
-	float* w_pitch = (float*)(0x086872C-4); 
+	float* w_pitch = (float*)(0x086872C - 4);
+	float* r_pitch = (float*)(0x074D60C - 4);
 
-	float* r_pitch = (float*)(0x074D60C-4);
+	float* read_yaw = (float*)0x0E0DAE0;
 
+	bool* IsGrounded = (bool*)0x0815F08;
 
-	if (KeyPressed(VK_INSERT)) {
-		
-		    guessNumber(500);
-			guessNumber(600);
-			guessNumber(555);
-
-	}
 
 
 	Sleep(3);
