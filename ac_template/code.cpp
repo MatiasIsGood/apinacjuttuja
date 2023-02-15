79ee3522c116a26d6592b946b4081ef2ae03ebb2
MatiasIsGood <76411418+MatiasIsGood@users.noreply.github.com>
2023-02-15 11:28:09 +0200

    main

--- ac_template/code.cpp
+++ ac_template/code.cpp
@@ -1,35 +1,47 @@
 #include "pch.h"
 
-struct vector3
-{
-	float x;
-	float y;
-	float z;
-};
-
-//todo teleport eteenpäin
-
-#define PITCH 0 //X
-#define YAW 1 //Y
-#define ROLL 2 //Z
+//en voi olla sit koko aja paikalla
+//asiakkaita  
+//juu
 
 bool ChangeGravity = false;
 
+//täällekö
+/*
+works
+mikä on console clear
 
-void guessNumber(int guess) {
-    
-	std::cout << guess  <<" : ";
-	if (guess == 555) {
-		std::cout << "Correct. You guessed it!\n";
-	}
-	else if (guess < 555) {
-		std::cout << "Your guess is too low.\n";
-	}
-	else if (guess > 555) {
-		std::cout << "Your guess is too high.\n";
-	}
+öööö ssi vrm -45 ja -135
+yes
+system("cls");
+*/
+int apina(float read_apina) {
+	system("cls");
 
+	std::cout << "nyt testaan anglea: " << read_apina << '\n'; //pinkki
+	if (read_apina >= 45 && read_apina < 135) { 
+		std::cout << "sehan on 45 ja 135 valilla kai??? " << '\n';
+		return 1;
+	}
+	else if (read_apina <= 45 && read_apina > -45) { //oranssi
+		std::cout << "sehan on 45 ja -45 valis kai??? " << '\n';
+		return 2;
+	}
+	else if (read_apina <= -45 && read_apina > -135) { //sinine
+		std::cout << "sehän on -45 ja -135 valis kai???" << '\n';
+		return 3;
+	}
+	else if (read_apina >= 135 || read_apina < -135) { //vihree
+		std::cout << "sehän on 135 ja -135 valis kai???" << '\n';
+		return 4;
+	}
+	 
+	std::cout << "ei ole alueella\n"; //voit vaa tehä näi for debugging
+	return 0; //hyvä
+
+	//tän koodin ei pitäs ikinä suorittaa, mut laita tänne joku return arvo mistä tiedät et tää kusi
 }
+
 
 void __main() {	
 
@@ -44,18 +56,36 @@ void __main() {
 	float* x = (float*)(0x0E0DA1C - 8); //eteenpäi
 
 	float* w_pitch = (float*)(0x086872C-4); 
-
 	float* r_pitch = (float*)(0x074D60C-4);
 
 
+	float* read_yaw = (float*)0x0E0DAE0;
+	
 	if (KeyPressed(VK_INSERT)) {
-		
-		    guessNumber(500);
-			guessNumber(600);
-			guessNumber(555);
+		int value = apina(*read_yaw); //anna vielä valuelle oikee datatyyppi
+
+		std::cout << value << '\n'; //toimiiii
+		//noni nyt 10 sijasta liiku tasan 0.125
+		//ja sit juokse seinää vaste ja testaa tätä
+		//jooo toimii jep oota sekka 
+		//epic games
+		//nyt tää broject on tehty
+
+		if (value == 1) { //vasen
+			*y += 0.125f; // huomio F lopussa <-- float 
+		}
+		else if (value == 2) { //ylös = x 
+			*x += 0.125f;
+		}
+		else if (value == 3) { //oikee eli = -y
+			*y -= 0.125f;
+		}
+		else if (value == 4) { // taakse eli = -x
+			*x -= 0.125f;
+		}
 
 	}
-
-
+	
+	
 	Sleep(3);
 }
